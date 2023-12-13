3f6d068de48d432f2c1c3efbda8b74a1e4f9fc89
Gustavo <ghpires23@stefanini.com>
2023-12-13 07:20:55 -0300

    config: subindo ambiente

--- infra/repository/usuario_repository.py
+++ infra/repository/usuario_repository.py
@@ -6,3 +6,20 @@ class UsuarioRepository():
         with DBConnectionHandler() as db:
             data = db.session.query(Usuario).all()
             return data
+    
+    def insert(self, nome, data_nascimento):
+            with DBConnectionHandler() as db:                 
+                data_insert = Usuario(nome=nome, data_nascimento=data_nascimento)
+                db.session.add(data_insert)
+                db.session.commit()
+
+    def delete(self, nome):
+            with DBConnectionHandler() as db:                 
+                db.session.query(Usuario).filter(Usuario.nome == nome).delete()
+                db.session.commit()
+
+    def update(self, nome):
+            with DBConnectionHandler() as db:                 
+                db.session.query(Usuario).filter(Usuario.nome == nome).update({"nome": nome})
+                db.session.commit()
+                                
--- run.py
+++ run.py
@@ -2,6 +2,8 @@ from infra.repository.usuario_repository import UsuarioRepository
 
 repo = UsuarioRepository()
 
+repo.insert('Henrique','10/10/2023')
+
 data = repo.select()
 
 print(data)
